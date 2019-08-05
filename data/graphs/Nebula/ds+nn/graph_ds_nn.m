2c73ac63f42cc4c375f91365b77f8af5835a6458
Joao Sa Sousa <jagsousa1992@gmail.com>
2019-08-05 12:14:27 +0200

    Added 3D graph for data size vs #nodes

--- data/graphs/Nebula/ds+nn/graph_ds_nn.m
+++ data/graphs/Nebula/ds+nn/graph_ds_nn.m
@@ -1,20 +1,18 @@
-X= repmat([1 5 10 25 50 100],[8,1]);
-Y= repmat([7 52 150 451 585 940 1366 1511]',[1,6]);
-Z=[773.32, 2060.64, 4364.54, 11387.88, 14367.74, 22074.04, 30283.7, 33912.3;
-   1795.56, NaN, 18274.42, NaN, 66450.3, NaN, 142159.9, NaN;
-   NaN, 14052.88, NaN, 101787.8, NaN, 203333.4, NaN, 314200.2;
-   6573.55, NaN, 87225.5, NaN, 325984.7, NaN, 701851.7, NaN;
-   NaN, 68134.1, NaN, 501835.9, NaN, 1021297.4, NaN, 1562792.0;
-   24194.7, NaN, 345820, NaN, 1302635.0, NaN, 2807409.2, NaN];
+X= repmat([3 6 9 12],[4,1]);
+Y= repmat([2.37 3.17 4.75 9.2]',[1,4]);
+Z= [75592.25, 100206.1, 153244.2, 314200.2;
+    76578.25, 101961.35, 152531.25, 315635.45;
+    76660.55, 102696.05, 154587.85, 318920.95;
+    77362.85, 102513.2, 155200.8, 321368.65];
 
 Z2 = abs(inpaint_nans(Z));
-[X3,Y3] = meshgrid(1:2:100,7:30:1511);
+[X3,Y3] = meshgrid(3:1:12,2.37:0.1:9.2);
 Z3 = interp2(X,Y,Z2',X3,Y3);
 Z3 = Z3/1000;
 surf(X3,Y3,Z3)
 %set(gca,'zscale','log')
 
 title('Total response time')
-xlabel('Number of query terms')
-ylabel('Size of the result set')
+xlabel('Number of nodes')
+ylabel('Size of the data set (billions of rows)')
 zlabel('Runtime (s)')
