20b781a65026ea24d1a1e45e0ea91e988271cea9
Joao Sa Sousa <jagsousa1992@gmail.com>
2019-08-05 13:11:45 +0200

    Restructured some of the graphs folder/files

--- data/graphs/Nebula/parser_ds+nn.py
+++ data/graphs/Nebula/parser_ds+nn.py
@@ -1,6 +1,6 @@
 import util
 
-sufix = ['20r_10q_1511p_3n_2.37B', '20r_10q_1511p_3n_3.17B', '20r_10q_1511p_3n_4.75B', '10r_10q_1511p_3n_9.5B']
+sufix = ['20r_10q_1511p_3n_2.37B', '20r_10q_1511p_3n_3.17B', '20r_10q_1511p_3n_4.75B', '10r_10q_1511p_3n_9.2B']
 filepath = "ds+nn/exp_"
 
 util.parse_data_set(filepath, sufix, 3)
